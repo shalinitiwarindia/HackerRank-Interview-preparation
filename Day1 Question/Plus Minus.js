d7a3ee0747d6618ef483eb0d15439ab740d14952
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-02-10 19:52:18 +0530

    Create Plus Minus.js

---
+++ Day1 Question/Plus Minus.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    let n = arr.length;
+    let positiveCount = 0, negativeCount = 0, zeroCount = 0;
+
+    for (let num of arr) {
+        if (num > 0) positiveCount++;
+        else if (num < 0) negativeCount++;
+        else zeroCount++;
+    }
+
+    console.log((positiveCount / n).toFixed(6));
+    console.log((negativeCount / n).toFixed(6));
+    console.log((zeroCount / n).toFixed(6));
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    plusMinus(arr);
+}
