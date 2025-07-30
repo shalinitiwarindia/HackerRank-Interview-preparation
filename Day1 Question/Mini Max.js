30ff910c99596a91ff4d06cdb10d8bdfa9465660
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-07-30 16:37:41 +0530

    Update Mini Max.js

--- Day1 Question/Mini Max.js
+++ Day1 Question/Mini Max.js
@@ -22,8 +22,7 @@ function readLine() {
 /*
  * Complete the 'miniMaxSum' function below.
  *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
+ 
 
 function miniMaxSum(arr) {
     arr.sort((a, b) => a - b); // Sort array in ascending order
