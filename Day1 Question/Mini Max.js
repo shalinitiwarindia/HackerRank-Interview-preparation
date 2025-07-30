6aadb66de9dad3d1e6eb3f9e7fcf500c58f945b2
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-07-30 16:38:18 +0530

    Update Mini Max.js

--- Day1 Question/Mini Max.js
+++ Day1 Question/Mini Max.js
@@ -6,17 +6,7 @@ process.stdin.setEncoding('utf-8');
 let inputString = '';
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-    main();
-});
-
-
-function miniMaxSum(arr) {
     arr.sort((a, b) => a - b); // Sort array in ascending order
 
 
