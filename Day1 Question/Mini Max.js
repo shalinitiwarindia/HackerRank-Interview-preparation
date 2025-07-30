2155c272eca27d1b751180e0430a178d3f58da8f
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-07-30 16:38:30 +0530

    Update Mini Max.js

--- Day1 Question/Mini Max.js
+++ Day1 Question/Mini Max.js
@@ -4,10 +4,7 @@ process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 
 let inputString = '';
-let currentLine = 0;
-
-
-    arr.sort((a, b) => a - b); // Sort array in ascending order
+le
 
 
 
